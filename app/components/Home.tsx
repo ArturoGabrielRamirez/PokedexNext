55938092f9532560f1a42257be559731881175ba
Arturo Ramirez <jiamafox@gmail.com>
2023-07-26 15:35:14 -0300

    Add PokeBall animation

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -12,34 +12,39 @@ function Home({ details }: { details: any }) {
                     let color = typesColorCards[item.types[0].type.name]
 
                     return (
-                        <div key={item.id} className='card'>
-                            <div className='flex relative rotate-cards-animation'>
+                        <div key={item.id} className='card group'>
+                            <div className='flex relative rotate-cards-animation w-full h-full'>
 
-                                <div className={`absolute w-full h-full border-8 border-yellow-200 rounded-md bg-gradient-to-b ${color} shadow-md back -z-10`}>
-                                    <div >
+                                <div className={`flex flex-col  h-full w-full border-8 border-yellow-200 rounded-md bg-gradient-to-b ${color} front`} >
 
+                                    <div className={`m-2 border-2 border-amber-300 rounded-sm bg-gradient-to-b ${color}`} >
+                                        <Image src={item.sprites.front_default} alt={item.name} width={200} height={200} />
                                     </div>
-                                    <div className={`w-full h-full bg-gradient-to-t ${color}`}>
-                                        <div />
-                                        <div />
+                                    <div className={`p-2 grow bg-gradient-to-t ${color}`}>
+                                        <h1 className='text-center text-2xl font-bold'>{item.name}</h1>
+                                        <div className='text-center border-2 rounded-full'>Height: {item.height}</div>
+                                        <div><ChipsTypes item={item} /></div>
+                                        <div className='text-center'>Weight: {item.weight}</div>
+                                    </div>
+                                </div>
+
+                                <div className={` absolute w-full h-full border-8 border-yellow-200 rounded-md bg-gradient-to-b ${color} back -z-10`}>
+
+                                    <div className={`flex items-center justify-center w-full h-full bg-gradient-to-t ${color}`}>
+                                        <div className='relative flex flex-col justify-center items-center group-hover:animate-bounce'>
+                                            <div className='bg-red-700 w-32 h-16 border-black text-white font-bold  border-4 rounded-t-full'>
+                                            </div>
+                                            <div className='absolute bg-white z-10 rounded-full border-black border-4 w-10 h-10 font-bold'></div>
+                                            <div className='border-black text-black bg-white font-bold w-32 h-16 border-4 rounded-b-full' />
+                                            <div />
+                                        </div>
                                     </div>
                                 </div>
 
-                                <div className={`border-8 border-yellow-200 rounded-md bg-gradient-to-b ${color}  shadow-md front`} >
-                                    <div>
-                                        <div className={`border-2 border-amber-300 m-2 rounded-sm bg-gradient ${color}`} >
-                                            <Image src={item.sprites.front_default} alt={item.name} width={200} height={200} />
-                                        </div>
-                                        <div className={`bg-gradient-to-t ${color}`}>
-                                            <h1 className='text-center text-2xl font-bold'>{item.name}</h1>
-                                            <div className='text-center border-2 rounded-full'>Height: {item.height}</div>
-                                            <div><ChipsTypes item={item} /></div>
-                                            <div className='text-center'>Weight: {item.weight}</div>
-                                        </div>
-                                    </div>
-                                </div>
+
                             </div>
-                        </div>)
+                        </div>
+                    )
                 })
 
             }
